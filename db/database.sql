6db2e930fbe6ae7efd8a8341b9ad912626cbd783
HECTOR MANJARREZ MACIAS <hectorhmm00@hotmail.com>
2022-10-03 22:07:53 -0600

    Primer prueba

---
+++ db/database.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE iF NOT EXISTS companydb;
+
+USE companydb;
+
+CREATE TABLE EMPLOYEE(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) DEFAULT NULL,
+    salary INT(5) DEFAULT NULL
+    PRIMARY KEY(id)
+);
+
+DESCRIBE EMPLOYEE;
+
+INSERT INTO EMPLOYEE VALUES
+    (1, 'Joe', 1000),
+    (2, 'Henry', 2000),
+    (3, 'Sam', 2500),
+    (4, 'Max', 1500);
